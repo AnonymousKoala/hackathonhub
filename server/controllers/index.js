4eea4d178670971325bff11b1930d6b30d71461a
Kevin Lui <kevin.lui@stonybrook.edu>
2018-12-06 12:35:21 -0500

    Added retrieve method to get a singular event object.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 const user = require('./user');
 const team = require('./team');
 const teamuser = require('./teamuser');
+const event = require('./event');
 
 module.exports = {
   user,
   team,
   teamuser,
+  event,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const userController = require('../controllers').user;
 const teamController = require('../controllers').team;
 const teamuserController = require('../controllers').teamuser;
+const eventController = require('../controllers').event;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -14,5 +15,9 @@ module.exports = (app) => {
   app.get('/api/teams', teamController.list);
 
   app.post('/api/teamuser/:teamID/:userID', teamuserController.create);
+
+  app.post('/api/events', eventController.create);
+  app.get('/api/events', eventController.list);
+  app.get('/api/events/:eventID', eventController.retrieve);
 
 };
